228f5a9d5421623386a28ab096972696a97019ae
joelsonRamos <joelson.engcomp@gmail.com>
2023-03-07 13:28:27 +0000

    finalizado

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container, Row, Col, Image, Button} from 'react-bootstrap';
 
 function App() {
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <header className="header">
+        <p className='cabecalho'>
+          404 NOT FOUND
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <Container className='App-header'>
+      
+        <Row className='linha'>
+          <Col xs={12} md={6} >
+            <Image className='image' src='../Scarecrow.png' fluid/>
+          </Col>
+          <Col xs={12} sm={12} md={6}>
+            <Col xs={12}  sm={6} md={6}>
+              <p className='titulo'>I have bad news for you</p>
+            </Col>
+            <Col xs={12}  sm={12} md={6}>
+              <p className='corpo_titulo'>The page you are looking for might be removed or is temporarily unavailable</p>
+            </Col>
+            <Col xs={12}  sm={6} md={6} className=''>
+              <Button className='float-left d-flex ' variant="dark" size="lg">Back to homepage</Button>
+            </Col>
+          
+          </Col>
+        </Row>
+      </Container>
+      <footer className='footer'>
+        <p className='texto-footer'>created by Joelson Ramos Ferreira - devChallenges.io</p>
+      </footer>
+
     </div>
   );
 }
